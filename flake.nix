b393df77d1c4afaae7e124105fcbd1988a1333f3
satoqz <satoqz@pm.me>
2022-12-08 07:38:17 +0100

    make flake work on linux

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-    in {
+    in rec {
       packages.default = pkgs.rustPlatform.buildRustPackage {
         name = "rapla-to-ics";
         version = "main";
@@ -19,8 +19,13 @@
         cargoLock.lockFile = ./Cargo.lock;
 
         buildInputs =
-          nixpkgs.lib.optional pkgs.stdenv.isDarwin
+          [pkgs.openssl]
+          ++ nixpkgs.lib.optional pkgs.stdenv.isDarwin
           pkgs.darwin.apple_sdk.frameworks.SystemConfiguration;
+
+        nativeBuildInputs = [pkgs.pkg-config];
       };
+
+      apps.default = flake-utils.lib.mkApp {drv = packages.default;};
     });
 }
