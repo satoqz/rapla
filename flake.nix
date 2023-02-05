5fc912edf717a5490119e649157f36894c0282de
satoqz <satoqz@pm.me>
2023-02-05 22:56:44 +0100

    fix darwin build

--- flake.nix
+++ flake.nix
@@ -1,89 +1,81 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nci = {
-      url = "github:yusdacra/nix-cargo-integration";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-overlay.follows = "rust-overlay";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     nixpkgs,
-    nci,
+    utils,
     ...
-  }: let
-    inherit (nixpkgs) lib;
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      name = "rapla-to-ics";
+      version = "0.2.0";
 
-    name = "rapla-to-ics";
+      inherit (nixpkgs) lib;
 
-    outputs = nci.lib.makeOutputs {
-      root = builtins.path {
-        inherit name;
-        path = ./.;
+      pkgs = import nixpkgs {
+        inherit system;
       };
 
-      config = common: {
-        outputs.defaults = {
-          package = name;
-          app = name;
+      nativeBuildInputs = [
+        pkgs.rustc
+        pkgs.cargo
+        pkgs.pkg-config
+      ];
+
+      buildInputs =
+        [pkgs.openssl]
+        ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
+          frameworks.Security
+        ]);
+    in rec {
+      packages.${name} = pkgs.rustPlatform.buildRustPackage {
+        inherit name version nativeBuildInputs buildInputs;
+
+        src = builtins.path {
+          inherit name;
+          path = ./.;
         };
 
-        shell.packages = with common.pkgs; [
-          treefmt
-          rust-analyzer
-          nil
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+
+      packages.default = packages.${name};
+
+      packages."${name}-docker" = pkgs.dockerTools.buildLayeredImage {
+        inherit name;
+        tag = "latest";
+
+        contents = [
+          packages.default
+          pkgs.cacert
         ];
 
-        cCompiler.package = common.pkgs.clang;
-      };
-
-      pkgConfig = common: {
-        ${name} = {
-          build = true;
-          app = true;
-
-          overrides.libraries.nativeBuildInputs = with common.pkgs;
-            [openssl libiconv]
-            ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-              CoreFoundation
-              Security
-              SystemConfiguration
-            ]);
+        config = {
+          Cmd = [name "serve"];
+          ExposedPorts."8080/tcp" = {};
         };
       };
-    };
-  in
-    outputs
-    // {
-      packages = lib.mapAttrs (system: packages: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-        packages
-        // lib.optionalAttrs pkgs.stdenv.isLinux {
-          "${name}-docker" = pkgs.dockerTools.buildLayeredImage {
-            inherit name;
-            tag = "latest";
 
-            contents = [
-              packages.default
-              pkgs.cacert
-            ];
+      formatter = pkgs.alejandra;
 
-            config = {
-              Cmd = [name];
-              ExposedPorts."8080/tcp" = {};
-            };
-          };
-        })
-      outputs.packages;
-    };
+      devShells.default = pkgs.mkShell {
+        inherit nativeBuildInputs buildInputs;
+        packages = [
+          # rust
+          pkgs.rustfmt
+          pkgs.rust-analyzer
+          pkgs.clippy
+          pkgs.cargo-watch
+
+          # nix
+          formatter
+          pkgs.nil
+        ];
+      };
+    });
 
   nixConfig = {
     extra-substituters = [
