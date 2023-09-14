76b29be9f6445001c413c74a635f0d0f2566db23
satoqz <mail@satoqz.net>
2023-09-14 23:13:14 +0200

    wip

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,24 +7,18 @@ import (
 	"github.com/satoqz/rapla-proxy/pkg/rapla"
 )
 
-func get(handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		handler(w, r)
-	}
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	cal := rapla.Extract("woo")
 	cal.SerializeTo(w)
 }
 
 func Serve() error {
-	http.HandleFunc("/", get(handler))
-
+	http.HandleFunc("/", handler)
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
