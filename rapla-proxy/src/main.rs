6c6fabd912faed860149ee6dd395367993170493
satoqz <mail@satoqz.net>
2024-05-15 12:19:02 +0200

    enhance: proxy calendar type

--- rapla-proxy/src/main.rs
+++ rapla-proxy/src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, env, io, net::SocketAddr, process, sync::Arc};
 
 use axum::{
-    extract::{Query, State},
+    extract::{Path, Query, State},
     http::StatusCode,
     response::{IntoResponse, Redirect, Response},
     routing::get,
@@ -13,10 +13,9 @@ use tokio::{net::TcpListener, sync::RwLock, task, time};
 
 use rapla_parser::{parse_calendar, Calendar};
 
-type Cache = Arc<RwLock<HashMap<String, Arc<Calendar>>>>;
+type Cache = Arc<RwLock<HashMap<(String, String), Arc<Calendar>>>>;
 
 const UPSTREAM: &str = "https://rapla.dhbw.de";
-const CALENDAR_PATH: &str = "/rapla/calendar";
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -31,7 +30,7 @@ async fn main() -> io::Result<()> {
     };
 
     let router = Router::new()
-        .route(CALENDAR_PATH, get(handle_calendar))
+        .route("/rapla/:calendar_type", get(handle_calendar))
         .fallback(|| async { Redirect::permanent(env!("CARGO_PKG_REPOSITORY")) })
         .with_state(Arc::new(RwLock::new(HashMap::new())));
 
@@ -52,13 +51,18 @@ struct CalendarQuery {
 
 async fn handle_calendar(
     State(cache): State<Cache>,
-    Query(query): Query<CalendarQuery>,
+    Path(calendar_type): Path<String>,
+    Query(CalendarQuery { key, salt, json }): Query<CalendarQuery>,
 ) -> Response {
-    let Some(calendar) = fetch_calendar(query.key, query.salt, cache).await else {
-        return (StatusCode::INTERNAL_SERVER_ERROR, "Failed proxy calendar").into_response();
+    let Some(calendar) = fetch_calendar_with_cache(calendar_type, key, salt, cache).await else {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to proxy calendar",
+        )
+            .into_response();
     };
 
-    if query.json {
+    if json {
         return Json(calendar.as_ref()).into_response();
     }
 
@@ -69,33 +73,47 @@ async fn handle_calendar(
         .into_response();
 }
 
-async fn fetch_calendar(key: String, salt: String, cache: Cache) -> Option<Arc<Calendar>> {
-    let now = Utc::now();
-    let year_ago = now - Duration::try_days(365).unwrap();
+async fn fetch_calendar_with_cache(
+    calendar_type: String,
+    key: String,
+    salt: String,
+    cache: Cache,
+) -> Option<Arc<Calendar>> {
+    let cache_key = (calendar_type, key);
 
-    let url = format!(
-        "{UPSTREAM}/{CALENDAR_PATH}?key={key}&salt={salt}&day={}&month={}&year={}&pages=104",
-        year_ago.day(),
-        year_ago.month(),
-        year_ago.year()
-    );
-
-    if let Some(calendar) = cache.read().await.get(&key) {
+    if let Some(calendar) = cache.read().await.get(&cache_key) {
         return Some(Arc::clone(calendar));
     }
 
-    let html = reqwest::get(&url).await.ok()?.text().await.ok()?;
+    let upstream_url = generate_upstream_url(&cache_key.0, &cache_key.1, &salt);
+    let html = reqwest::get(upstream_url).await.ok()?.text().await.ok()?;
     let calendar = Arc::new(parse_calendar(&html)?);
 
     cache
         .write()
         .await
-        .insert(key.clone(), Arc::clone(&calendar));
+        .insert(cache_key.clone(), Arc::clone(&calendar));
 
     task::spawn(async move {
         time::sleep(time::Duration::from_secs(60 * 60)).await;
-        cache.write().await.remove(&key);
+        cache.write().await.remove(&cache_key);
     });
 
     Some(calendar)
+}
+
+fn generate_upstream_url(calendar_type: &str, key: &str, salt: &str) -> String {
+    // these don't need to be 100% accurate
+    const WEEKS_TWO_YEARS: usize = 104;
+    const DAYS_ONE_YEAR: i64 = 365;
+
+    let now = Utc::now();
+    let year_ago = now - Duration::try_days(DAYS_ONE_YEAR).unwrap();
+
+    format!(
+        "{UPSTREAM}/rapla/{calendar_type}?key={key}&salt={salt}&day={}&month={}&year={}&pages={WEEKS_TWO_YEARS}",
+        year_ago.day(),
+        year_ago.month(),
+        year_ago.year()
+    )
 }
